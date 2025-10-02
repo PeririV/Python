de2c7b6d6163254a16b224e191a64f045f25dcde
Сейчас Перерыв <ianchristopherpersonal@gmail.com>
2025-10-02 22:22:54 +0300

    Exercise 17 Secound exercise its okay

--- Excer16 e 17.py
+++ Excer16 e 17.py
@@ -1,7 +1,7 @@
 # 16 - Faça um algoritmo que leia três valores que representam os três lados de um triângulo e verifique se são válidos, determine se o triângulo é
 # equilátero, isósceles ou escaleno.
 
-
+"""
 A = int(input("Insira o valor (em Graus) de A: "))
 B = int(input("Insira o valor (em Graus) de B: "))
 C = int(input("Insira o valor (em Graus) de C: "))
@@ -15,3 +15,18 @@ elif A != B and A != C and B != C:
     print("ESCALENO")
 else:
     print("Chapa não parceiro")
+
+"""
+
+
+
+
+#  17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+# Fórmula: C = (5 * ( F-32) / 9)
+
+
+Temp = float(input("Insira o valor de Fahrenheit: "))
+
+Celsius = (5 * (Temp - 32) / 9)
+
+print(Celsius)

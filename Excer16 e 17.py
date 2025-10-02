55d564366198ae808f79a34fd4a79e897af4af21
Сейчас Перерыв <ianchristopherpersonal@gmail.com>
2025-10-02 22:18:20 +0300

    Exercise 17 Need to fix and avoid errors

---
+++ Excer16 e 17.py
@@ -0,0 +1,17 @@
+# 16 - Faça um algoritmo que leia três valores que representam os três lados de um triângulo e verifique se são válidos, determine se o triângulo é
+# equilátero, isósceles ou escaleno.
+
+
+A = int(input("Insira o valor (em Graus) de A: "))
+B = int(input("Insira o valor (em Graus) de B: "))
+C = int(input("Insira o valor (em Graus) de C: "))
+
+
+if A == B and A == C and B == C:
+    print("EQUILÀTERO")
+elif A == B and A != C or B == C and A != C:
+    print("ISÓCELES")
+elif A != B and A != C and B != C:
+    print("ESCALENO")
+else:
+    print("Chapa não parceiro")

3ce9a6a1fda8f55d379c8d45b7d6d8ff50a7ba49
Сейчас Перерыв <ianchristopherpersonal@gmail.com>
2025-10-14 21:08:24 +0300

    Colors added

--- Disponibilidade de cadeiras.py
+++ Disponibilidade de cadeiras.py
@@ -4,10 +4,10 @@
 # Liste todas as cadeiras disponíveis
 # Liste todas as cadeiras indisponíveis
 # Calcule e mostre estatísticas totais"
+import random
 
 from colorama import Fore, Back, Style
-
-
+import random
 linha = ["A", "B", "C", "D"]
 
 #for i in linha:
@@ -17,10 +17,13 @@ print(Fore.GREEN + "--" * 30)
 texto = "-- Cinema com filmes --"
 print(Fore.RED + Back.BLACK + texto.center(60) + Style.RESET_ALL)
 print(Fore.GREEN + "--" * 30 + Style.RESET_ALL)
-
+slot = [Fore.GREEN , Fore.RED ]
 
 for i in linha:
     for num in range(1,6):
         idt = f"{i}{num}"
+        color = random.choice(slot)
+        print(color)
         print(idt.center(12), end=" ")
+        print(Style.RESET_ALL, end=" ")
     print()

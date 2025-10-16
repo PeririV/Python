14d987e76fe03c5334a0eafbfb35ee2c60b00ada
Сейчас Перерыв <ianchristopherpersonal@gmail.com>
2025-10-16 16:36:46 +0300

    Organização ou o inverso?

--- Disponibilidade de cadeiras.py
+++ Disponibilidade de cadeiras.py
@@ -17,10 +17,26 @@
 # Cancelar reservas
 # Ver histórico de operações
 # Salvar/recuperar estado
-from fileinput import close
 
 from colorama import Fore, Back, Style
 import random
+
+linha = ["A", "B", "C", "D"]
+
+#cadeiras = {}
+busy = []
+free = []
+
+def escolha_assento():
+    intro = "Olá, Seja Muito Bem Vindo."
+    intro2 = "Escolha o Assento Desejado, Ex: (A1, B2, C3...): "
+    Cchosed = str(input(Fore.LIGHTYELLOW_EX + Back.BLACK + intro.center(
+        60) + Style.RESET_ALL + "\n" + Fore.LIGHTYELLOW_EX + Back.BLACK + intro2.center(
+        60) + Style.RESET_ALL + "\nAssentos: ")).capitalize().split(",")
+    return
+
+def exit():
+    print("Saindo do sistema...")
 
 
 def menu():
@@ -50,8 +66,35 @@ def menu():
             continue
 
         if option == 1:
+            countR = 0
+            countG = 0
+            print(Fore.GREEN + "--" * 30)
+            texto = "-- Cinema com filmes --"
+            print(Fore.RED + Back.BLACK + texto.center(60) + Style.RESET_ALL)
+            print(Fore.GREEN + "--" * 30 + Style.RESET_ALL)
+            slot = [Fore.GREEN, Fore.RED]
+            for i in linha:
+                for num in range(1, 6):
+                    idt = f"{i}{num}"
+                    color = random.choice(slot)
+                    if color == Fore.RED:
+                        countR += 1
+                        busy.append(idt)
+                    else:
+                        countG += 1
+                        free.append(idt)
+                    print(color, idt.center(10), end=" ")
+                    print(Style.RESET_ALL, end=" ")
+                print()
 
+                perctf = (countG / (countR + countG)) * 100
+                perctb = (countR / (countR + countG)) * 100
+                print(f"Quantidade de Lugares Disponiveis: {slot[0]}{countG}" + Style.RESET_ALL)
+                print(f"Cadeiras livres: {slot[0]}{perctf:.0f}%\n{free}\n" + Style.RESET_ALL)
+                print(f"Quantidade de Lugares Ocupados: {slot[1]}{countR}" + Style.RESET_ALL)
+                print(f"Cadeiras Ocupadas: {slot[1]}{perctb:.0f}%\n{busy}\n" + Style.RESET_ALL)
             return
+
         elif option == 2:
             escolha_assento()
         elif option == 3:
@@ -71,53 +114,15 @@ def menu():
     return int(option)
 
 
-menu()
+#menu()
 
 
 
-def escolha_assento():
-    intro = "Olá, Seja Muito Bem Vindo."
-    intro2 = "Escolha o Assento Desejado, Ex: (A1, B2, C3...): "
-    Cchosed = str(input(Fore.LIGHTYELLOW_EX + Back.BLACK + intro.center(
-        60) + Style.RESET_ALL + "\n" + Fore.LIGHTYELLOW_EX + Back.BLACK + intro2.center(
-        60) + Style.RESET_ALL + "\nAssentos: ")).capitalize().split(",")
-    return
 
-def exit():
-    print("Saindo do sistema...")
 
-linha = ["A", "B", "C", "D"]
 
-print(Fore.GREEN + "--" * 30)
-texto = "-- Cinema com filmes --"
-print(Fore.RED + Back.BLACK + texto.center(60) + Style.RESET_ALL)
-print(Fore.GREEN + "--" * 30 + Style.RESET_ALL)
-slot = [Fore.GREEN , Fore.RED ]
-escolha_assento()
-countR = 0
-countG = 0
-#cadeiras = {}
-busy = []
-free = []
 
-for i in linha:
-    for num in range(1,6):
-        idt = f"{i}{num}"
-        color = random.choice(slot)
-        if color == Fore.RED:
-            countR += 1
-            busy.append(idt)
-        else:
-            countG += 1
-            free.append(idt)
-        print(color, idt.center(10), end=" ")
-        print(Style.RESET_ALL, end=" ")
-    print()
-perctf = (countG / (countR + countG)) * 100
-perctb = (countR / (countR + countG)) * 100
-print(f"Quantidade de Lugares Disponiveis: {slot[0]}{countG}" + Style.RESET_ALL)
-print(f"Cadeiras livres: {slot[0]}{perctf:.0f}%\n{free}\n" + Style.RESET_ALL)
-print(f"Quantidade de Lugares Ocupados: {slot[1]}{countR}" + Style.RESET_ALL)
-print(f"Cadeiras Ocupadas: {slot[1]}{perctb:.0f}%\n{busy}\n" + Style.RESET_ALL)
+
+
 
 
